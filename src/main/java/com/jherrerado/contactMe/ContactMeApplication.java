8659c877efed01b1713a7208d49c1cb988a37416
jherrera <jhan.joshua.h.g@gmail.com>
2024-12-25 17:20:21 -0400

    Project start

---
+++ src/main/java/com/jherrerado/contactMe/ContactMeApplication.java
@@ -0,0 +1,13 @@
+package com.jherrerado.contactMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jherrerado/contactMe/ContactMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jherrerado.contactMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
