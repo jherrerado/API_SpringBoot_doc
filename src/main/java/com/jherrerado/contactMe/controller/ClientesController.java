4837abd544ea6b97e5677acd48cd91c4e0489bd7
jherrera <jhan.joshua.h.g@gmail.com>
2024-12-26 18:16:50 -0400

    lastest details about project architecture

--- src/main/java/com/jherrerado/contactMe/controller/ClientesController.java
+++ src/main/java/com/jherrerado/contactMe/controller/ClientesController.java
@@ -6,12 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import com.jherrerado.contactMe.model.dto.ClientesDTO;
+import com.jherrerado.contactMe.model.entities.Clientes;
 import com.jherrerado.contactMe.model.mapper.ClientesMapper;
 import com.jherrerado.contactMe.services.IClientesServices;
 
 @RestController
 @RequestMapping("/api/v1")
 
+/*
+ * Controller lo que hace ya es llamar a services, en este caso a
+ * IClientesServices
+ */
 public class ClientesController {
 
   @Autowired
@@ -20,6 +25,11 @@ public class ClientesController {
   @GetMapping("/clientes")
   public List<ClientesDTO> getClientes() {
     return ClientesMapper.INSTANCE.clientesToClientesDTOs(clientesServices.findClientes());
+  }
+
+  @GetMapping("/pruevas")
+  public List<Clientes> getPClientes() {
+    return clientesServices.findClientes();
   }
 
 }
